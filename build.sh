963acb0f61e46a900f118241471998e4d26c30d7
soltia48 <soltia48@protonmail.com>
2024-01-28 03:04:50 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname "$0")"
+
+if [ -n "$(docker ps -f "name=dict-build" -f "status=running" -q )" ]; then
+    docker stop dict-build
+fi
 
 docker build -t merge-ut-dictionaries ./
 docker create -it --rm --name dict-build merge-ut-dictionaries
@@ -13,6 +19,7 @@ docker exec dict-build ./scripts/build-dict.sh
 
 # Copy built assets
 mkdir -p ./dist/
+rm -f ./dist/*
 docker cp dict-build:/home/debian/merge-ut-dictionaries/src/mozcdic-ut.txt ./dist/
 
 docker stop dict-build
